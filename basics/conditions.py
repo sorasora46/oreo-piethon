fd7133f9f2eb2d836a13b855135f8a3284e71f8c
sora-k46 <sorrawit456789@gmail.com>
2023-06-25 23:09:13 +0700

    basic condition and while, for loops

--- basics/conditions.py
+++ basics/conditions.py
@@ -1,8 +1,19 @@
 age = int(input('What is your age?: '))
 
+# and + elif + else
 if age >= 18:
     print('Legal')
 elif age >= 16 and age < 18:
     print('Maybe Legal')
 else:
     print('Illegal')
+
+# or
+num = int(input('Enter number: '))
+
+if num == 20 or num % 2 == 0:
+    print('Cool')
+
+# not
+if not(num % 2 == 0):
+    print('Can\'t devided by 2')
---
+++ basics/loops.py
@@ -0,0 +1,52 @@
+import time
+# while loop
+# execute as long as condition is true
+
+n = int(input('Enter number: '))
+
+i = 1
+# count number to n
+while i <= n:
+  print('index:', i)
+  i += 1
+
+str = input('Enter string less than 5 characters: ')
+if len(str) < 5:
+  l = 0
+  while l < len(str):
+    print(str[l])
+    l += 1
+
+name = '' 
+while len(name) == 0:
+  name = input('Enter your name: ')
+print(name)
+# or u can do this
+# name = None
+# while not name:
+
+# for loop
+# execute with defined condition (already know how many time to iterate)
+for index in range(10):
+  print(index)
+
+for index in range(10, 50): # range(inclusive, exclusive)
+  print(index)
+
+for index in range(10, 50, 2): # count up by 2
+  print(index)
+
+for index in range(10, 50, -2): # count down by 2
+  print(index)
+
+for index in 'Hi, My name is Boss.':
+  print(index)
+
+for second in range(10, 0, -1):
+  print(second)
+  time.sleep(1) # in second
+
+for m in range(10):
+  for n in range(10):
+    print('#', end='')
+  print(end='\n')
