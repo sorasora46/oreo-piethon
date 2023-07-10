ee5c90ba81b16974a389d14918614646854d4470
sora <sorrawit02546@gmail.com>
2023-07-11 00:02:11 +0700

    learn module system

---
+++ modules/greeting.py
@@ -0,0 +1,14 @@
+def hi():
+    print('hi')
+
+def hello():
+    print('hello')
+
+def good_morning():
+    print('good morning')
+
+def good_afternoon():
+    print('good afternoon')
+
+def good_evening():
+    print('good evening')
---
+++ modules/test_module.py
@@ -0,0 +1,8 @@
+# module = a file containing python code. May contain functions, classes, etc.
+# used with modular programming, which is to separate a program into parts
+
+import greeting as gt
+
+gt.good_afternoon()
+gt.hi()
+gt.hello()
