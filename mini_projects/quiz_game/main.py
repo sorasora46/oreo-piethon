5385f0fdcc0f73d4e21f7c36bb60f71037661a69
sora <sorrawit02546@gmail.com>
2023-07-12 22:59:27 +0700

    init quiz game

---
+++ mini_projects/quiz_game/main.py
@@ -0,0 +1,10 @@
+quizes = [
+    ['Question 1', ['Ans 1', 'Ans 2', 'Ans 3', 'Ans 4']],
+    ['Question 2', ['Ans 1', 'Ans 2', 'Ans 3', 'Ans 4']],
+    ['Question 3', ['Ans 1', 'Ans 2', 'Ans 3', 'Ans 4']],
+    ['Question 4', ['Ans 1', 'Ans 2', 'Ans 3', 'Ans 4']],
+    ['Question 5', ['Ans 1', 'Ans 2', 'Ans 3', 'Ans 4']],
+]
+
+def play():
+    print('play')
