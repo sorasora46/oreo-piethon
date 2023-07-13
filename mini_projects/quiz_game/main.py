fff4af99bad441de2069191a9297ed88973fa2ce
sora <sorrawit02546@gmail.com>
2023-07-13 21:21:16 +0700

    quiz game

--- mini_projects/quiz_game/main.py
+++ mini_projects/quiz_game/main.py
@@ -6,5 +6,33 @@ quizes = [
     ['Question 5', ['Ans 1', 'Ans 2', 'Ans 3', 'Ans 4']],
 ]
 
+answers = [1, 2, 2, 3, 4]
+
+def calculate_score(user_ans):
+    score = 0
+    for i in range(len(user_ans)):
+        if user_ans[i] == answers[i]:
+            score += 1
+    return score
+
+def prompt_quiz(quiz):
+    print('-'*40)
+    print(quiz[0])
+    print()
+    for i in range(4):
+        print('[' + str(i + 1) + '] ' + quiz[1][i]) 
+    print()
+    print('-'*40)
+
 def play():
-    print('play')
+    user_ans = [] 
+    for i in range(len(quizes)):
+        prompt_quiz(quizes[i])
+        ans = 0
+        while ans < 1 or ans > 4:
+            ans = int(input('Your answer: '))
+        user_ans.append(ans)
+    score = calculate_score(user_ans)
+    print('Your score is ' + str(score))
+
+play()
